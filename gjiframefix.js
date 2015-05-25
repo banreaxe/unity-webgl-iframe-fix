a8b47a295a75e367c6c498561ce46f79161d6d31
Mark Koetse <mkoetse@gmail.com>
2015-05-24 22:51:10 -0600

    initial commit

---
+++ gjiframefix.js
@@ -0,0 +1,9 @@
+<!-- to overcome gamejolt focus issue !-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script>
+    $(window).on('load', function(){
+	//$(document).ready(function(){
+    //$(parent.document).find('iframe').focus();
+	$(document).find('iframe').focus();
+    })
+</script>
